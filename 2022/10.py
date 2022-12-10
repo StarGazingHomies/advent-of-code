6cefbc5c550808f27571a0d4713b787011013ec4
StarGazingHomies <96637509+StarGazingHomies@users.noreply.github.com>
2022-12-10 00:29:41 -0500

    Day 10 Readability Update

--- 2022/10.py
+++ 2022/10.py
@@ -1,40 +1,29 @@
+def cycle(increment = 0):
+    global prgmCnt, result, interest, X
+    if abs(prgmCnt % 40 - X) <= 1:
+        print('#', end='')
+    else:
+        print('.', end='')
+    if prgmCnt % 40 == 39:
+        print()
+    prgmCnt += 1
+    if prgmCnt in interest:
+        result += X * prgmCnt
+    X += increment
+
 if __name__ == '__main__':
     with open("10.in","r") as fin:
         lns = fin.readlines()
 
-    prgmCnt = -1
+    prgmCnt = 0
     X = 1
     result = 0
     interest = [20,60,100,140,180,220]
     for ln in lns:
         if ln[:4] == "noop":
-            prgmCnt += 1
-##            print(prgmCnt, X, abs(prgmCnt % 40 - X), abs(prgmCnt % 40 - X)<=1)
-            if abs(prgmCnt % 40 - X) <= 1:
-                print('#', end='')
-            else:
-                print('.', end='')
-            if prgmCnt % 40 == 39:
-                print()
+            cycle()
         else:
-            prgmCnt += 1
-##            print(prgmCnt, X, abs(prgmCnt % 40 - X), abs(prgmCnt % 40 - X)<=1)
-            if abs(prgmCnt % 40 - X) <= 1:
-                print('#', end='')
-            else:
-                print('.', end='')
-            if prgmCnt % 40 == 39:
-                print()
-                
-            prgmCnt += 1
-##            print(prgmCnt, X, abs(prgmCnt % 40 - X), abs(prgmCnt % 40 - X)<=1)
-            if abs(prgmCnt % 40 - X) <= 1:
-                print('#', end='')
-            else:
-                print('.', end='')
-            if prgmCnt % 40 == 39:
-                print()
-  
-            X += int(ln[5:])  
+            cycle()
+            cycle(int(ln[5:]))
         
-    print(prgmCnt, X)        
+    print("Part 1:", result)

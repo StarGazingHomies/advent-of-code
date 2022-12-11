ab8608a0371813b1e1d4a54b07d895a22f82fe4a
StarGazingHomies <96637509+StarGazingHomies@users.noreply.github.com>
2022-12-11 00:52:50 -0500

    Day 11 Abstraction & Readability

--- 2022/11.py
+++ 2022/11.py
@@ -1,49 +1,59 @@
+def ints(line):
+    i = 0
+    rtrns = []
+    while i < len(line):
+        if line[i] in ['1','2','3','4','5','6','7','8','9','0']:
+            j = i + 1
+            while j < len(line) and line[j] in ['1','2','3','4','5','6','7','8','9','0']:
+                j += 1
+            
+            rtrns.append(int(line[i:j]))
+            i = j
+        i += 1
+    return rtrns
 
+class Monkey():
+    def __init__(self, items, op, check, true, false):
+        self.items = items
+        self.op = op
+        self.check = check
+        self.true = true
+        self.false = false
+
+    def __str__(self):
+        return f"Monkey<op: {op}, check: {check}, true: {true}, false: {false}, items: {items}>"
 
 if __name__ == '__main__':
     with open("11.in","r") as fin:
         lns = fin.readlines()
-
-    monkeyItems = []
-    monkeyCondition = []
-    monkeyOp = []
-    monkeyCheck = []
-    monkeyTrueOp = []
-    monkeyFalseOp = []
+    
+    monkeys = []
+    
     for i in range(0,len(lns),7):
-        monkeyIndex = int(lns[i][7:-2])
-        monkeyItems.append(eval("["+lns[i+1][17:]+"]"))
-        monkeyOp.append(lns[i+2][13:][:-1]) # Use exec(operation)
-        monkeyCheck.append(int(lns[i+3][21:]))
-        monkeyTrueOp.append(int(lns[i+4][29:-1]))
-        monkeyFalseOp.append(int(lns[i+5][30:-1]))
-        # print(monkeyIndex, monkeyList, operation, check)
-    print(monkeyTrueOp, monkeyFalseOp)
+        # Items, Op, Check, True, False (in that order)
+        monkeys.append(Monkey(ints(lns[i+1]), lns[i+2][13:-1], ints(lns[i+3])[0], ints(lns[i+4])[0], ints(lns[i+5])[0]))
 
-    inspectCnt = [0 for i in range(len(monkeyItems))]
+    # Want to be able to sort it
+    inspectCnt = [0 for i in range(len(monkeys))]
 
     for i in range(10000):
-##        print(f"Round {i+1}")
-        newMonkeyItems = [[] for _ in range(len(monkeyItems))]
-        for i in range(len(monkeyItems)):
-##            print(f"Monkey {i}:")
-##            print(f"MonkeyItems {monkeyItems[i]}")
-            while len(monkeyItems[i]) > 0:
+        for i in range(len(monkeys)):
+            curMonkey = monkeys[i]
+            while len(curMonkey.items) > 0:
                 inspectCnt[i]+=1
-                item = monkeyItems[i].pop(0)
-##                print("Old:",item)
+                item = curMonkey.items.pop(0)
+                # Go down the VERY UNSAFE, DONT USE IN ACTUAL CODE route
                 old = item
-                exec(monkeyOp[i])
+                exec(curMonkey.op)
+
+                # Modulo for prt 2, so numbers are manageable
                 new = new % 9699690
-##                print("New:",new)
-                if (new % monkeyCheck[i] == 0):
-##                    print("Thrown to", monkeyTrueOp[i])
-                    monkeyItems[monkeyTrueOp[i]].append(new)
+
+                # Throw the item accordingly
+                if (new % curMonkey.check == 0):
+                    monkeys[curMonkey.true].items.append(new)
                 else:
-##                    print("Thrown to", monkeyFalseOp[i])
-                    monkeyItems[monkeyFalseOp[i]].append(new)
-##            print(monkeyItems)
-##            print("----------------")
+                    monkeys[curMonkey.false].items.append(new)
     print(inspectCnt)
     inspectCnt.sort()
     print(inspectCnt)
